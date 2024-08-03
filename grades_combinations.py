ed3a66e09192267f073c19fe0b017fc45e01e9a7
Bernard Itoro <bernarditoro@gmail.com>
2024-08-03 06:57:39 +0100

    - Increased input to 7 courses - Increased the range of grades combination by finding the permutation of all combinations of the grades - Implemented export of grades to a txt file

--- grades_combinations.py
+++ grades_combinations.py
@@ -1,4 +1,4 @@
-from itertools import combinations_with_replacement
+from itertools import combinations_with_replacement, permutations
 
 from tabulate import tabulate
 
@@ -7,32 +7,40 @@ grade_points = {'A': 5, 'B': 4, 'C': 3, 'D': 2}
 
 grades = (s.upper() for s in 'abcd')
 
-reply = """\nSo this initial script is built with stern constraints for testing purposes which are:
+reply = """\nSo I've expanded the script to work with more input (though hardcoded):
 
-* You offer 4 courses:
-    CSC111 (3 units)
-    STA111 (3 units)
-    GST111 (2 units)
-    PHY117 (1 unit)
+* You offer 7 courses
 * You want to get the best combination of grades that will give you a minimum CGPA of 4.50 (in a system of 5.0 CGPA)
 * You want to completely avoid Es and Fs
 * CGPA = sum(units * grade points) / sum(units)\n"""
 
 print(reply)
 
-courses_tuple = ('CSC111', 'STA111', 'GST111', 'PHY117')
-courses_units = (3, 3, 2, 1)
+courses_tuple = ('COS121', 'GST121', 'MTH121', 'PHY121', 'PHY128', 'CSC121', 'CSC122')
+courses_units = (3, 2, 2, 2, 1, 3, 2)
 
-grades_combinations = list(combinations_with_replacement(grades, 4))
+grades_combinations = list(combinations_with_replacement(grades, len(courses_units)))
 
-preferred_grades_combination = [] # This contains the grade combinations in tuples
+all_grades_combinations = []
 
-for grades_combination in grades_combinations:
+for combo in grades_combinations:
+    all_grades_combinations += list(permutations(combo))
+ 
+all_grades_combinations = list(set(all_grades_combinations))
+
+preferred_grades_combination = []
+
+for grades_combination in all_grades_combinations:
     credit_hours = sum([s * grade_points[y] for s, y in zip(courses_units, grades_combination)])
     credit_units = sum(courses_units)
 
     if credit_hours/credit_units > 4.5:
         preferred_grades_combination.append(grades_combination)
 
-print (tabulate(preferred_grades_combination, headers=courses_tuple, tablefmt='grid'))
+table = tabulate(preferred_grades_combination, headers=courses_tuple, tablefmt='grid')
+
+with open('grades.txt', 'w') as f:
+    f.write(table)
+
+print('Table written to grades.txt!')
 
