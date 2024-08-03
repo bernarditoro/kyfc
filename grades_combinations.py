d2be9ca0455d3b0cb964a0667c15a10360e0cbd9
Bernard Itoro <bernarditoro@gmail.com>
2024-08-03 09:32:14 +0100

    - Updated the table to show the GPA - Changed variable names where appropriate

--- grades_combinations.py
+++ grades_combinations.py
@@ -2,8 +2,10 @@ from itertools import combinations_with_replacement, permutations
 
 from tabulate import tabulate
 
+import math
 
-grade_points = {'A': 5, 'B': 4, 'C': 3, 'D': 2}
+
+grade_points = {'A': 5, 'B': 4, 'C': 3, 'D': 2, 'E': 1}
 
 grades = (s.upper() for s in 'abcd')
 
@@ -16,10 +18,10 @@ reply = """\nSo I've expanded the script to work with more input (though hardcod
 
 print(reply)
 
-courses_tuple = ('COS121', 'GST121', 'MTH121', 'PHY121', 'PHY128', 'CSC121', 'CSC122')
-courses_units = (3, 2, 2, 2, 1, 3, 2)
+courses_list = ['COS121', 'GST121', 'MTH121', 'PHY121', 'PHY128', 'CSC121', 'CSC122', 'GPA']
+credit_hours = (3, 2, 2, 2, 1, 3, 2)
 
-grades_combinations = list(combinations_with_replacement(grades, len(courses_units)))
+grades_combinations = list(combinations_with_replacement(grades, len(credit_hours)))
 
 all_grades_combinations = []
 
@@ -31,13 +33,21 @@ all_grades_combinations = list(set(all_grades_combinations))
 preferred_grades_combination = []
 
 for grades_combination in all_grades_combinations:
-    credit_hours = sum([s * grade_points[y] for s, y in zip(courses_units, grades_combination)])
-    credit_units = sum(courses_units)
+    quality_points = sum([s * grade_points[y] for s, y in zip(credit_hours, grades_combination)])
+    sum_credit_hours = sum(credit_hours)
 
-    if credit_hours/credit_units > 4.5:
-        preferred_grades_combination.append(grades_combination)
+    gpa = quality_points / sum_credit_hours
 
-table = tabulate(preferred_grades_combination, headers=courses_tuple, tablefmt='grid')
+    # Since GPAs are always truncated after 2 decimals...
+    gpa = float(f"{math.trunc(gpa)}.{str(gpa).split('.')[1][:2]}")
+
+    if gpa > 4.5:
+        grades_combination_list = list(grades_combination)
+        grades_combination_list.append(gpa)
+
+        preferred_grades_combination.append(grades_combination_list)
+
+table = tabulate(preferred_grades_combination, headers=courses_list, tablefmt='grid')
 
 with open('grades.txt', 'w') as f:
     f.write(table)
